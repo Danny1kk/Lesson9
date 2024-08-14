9097be6f8de965a81ac47176e09152d65fd8e0e6
Danny1kk <danny.koshik@gmail.com>
2024-08-14 09:59:15 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+print('Ввод в консоль: ')
+first = input()
+second = input()
+third = input()
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
